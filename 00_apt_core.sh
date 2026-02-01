da779957208f09623722bd8007eea882107b8954
monapo @ ubuntu <mopapo@the.quietest.linux>
2026-02-02 07:06:53 +0900

    new 00_apt_core

---
+++ 00_apt_core.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#
+# for ubuntu
+# 
+# curl <Remote> | sudo bash
+#
+
+set -e
+
+# 管理者権限が必要な作業
+## sudo で実行されているかチェック
+if [ "$EUID" -ne 0 ]; then
+    echo "警告: /opt への展開には管理者権限が必要。"
+    echo "sudo をつけて再実行してください。"
+    echo "Usage:"
+    echo "curl -fsSL <Remote> | sudo bash
+    exit 1
+fi
+
+apt update && apt upgrade -y
+apt install git python3 zsh
+
+mkdir -p ~/download && cd ~/download
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+
+rm -rf /opt/nvim-linux-x86_64
+tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+ln -sf /opt/nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim
+
+echo "Neovim のインストールが完了！"
+
+
