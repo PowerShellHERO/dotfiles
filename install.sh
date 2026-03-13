1ae29112d8df012e6a834072ee807a3cf535fa13
monapo @ ubuntu <mopapo@the.quietest.linux>
2026-03-14 02:36:59 +0900

    init: install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+sudo -v
+
+## Install 方法:
+## -   このアドレスを `curl | bash`
+## -   これを install.sh に保存して chmod 755 install -> run,
+## -   この下を逐次実行
+
+sudo apt update
+sudo apt install -y git curl
+
+
+sh -c "$(curl -fsLS get.chezmoi.io)"
+
+chezmoi init https://github.com/PowerShellHERO/dotfiles.git
+chezmoi apply
+
