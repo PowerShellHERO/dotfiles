bd6d0b3ac5e5b5945a1f0c0f90daae1c9f712f27
monapo @ ubuntu <mopapo@the.quietest.linux>
2026-03-14 11:50:13 +0900

    init: after_00_apt.sh

---
+++ run_once_after_00_apt.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+sudo -v
+
+sudo apt install -y \
+    locales \
+    python3 \
+    tmux
+
+
+# locales
+locale-gen en_US.UTF-8
+update-locale LANG=en_US.UTF-8
+
+# nvim {{{
+mkdir -p ~/download && cd ~/download
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+
+rm -rf /opt/nvim-linux-x86_64
+tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+ln -sf /opt/nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim
+cd
+# }}}
+
+# zsh plugin
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_PLUGINS/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_PLUGINS/plugins/zsh-autosuggestions
+
+## Starship
+curl -sS https://starship.rs/install.sh | sh
+
+source "$HOME"/.zshenv
+source "$ZDOTDIR"/.zshrc
+
+
