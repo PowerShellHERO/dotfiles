ff8aa2cd2fc5fbcd72932e0ffe3785904df01c08
monapo @ ubuntu <mopapo@the.quietest.linux>
2026-03-14 11:19:13 +0900

    init: run_once_mkdir

---
+++ run_once_mkdir.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 1. XDG 基本変数の定義
+# すでに環境変数が設定されている場合はそれを使う。
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
+export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
+
+export HISTFILE="$XDG_STATE_HOME"/zsh/history
+export ZDOTDIR="$XDG_CONFIG_HOME"/zsh
+export ZSH_PLUGINS="$ZDOTDIR"/plugins
+
+# mkdir
+mkdir "$XDG_CONFIG_HOME/zsh"
+mkdir "$XDG_CONFIG_HOME/git"
+mkdir "$XDG_DATA_HOME/fonts"
+mkdir "$XDG_STATE_HOME/zsh"
+mkdir "$XDG_CACHE_HOME/zsh"
+mkdir "$HOME/.local/bin"
+mkdir $ZSH_PLUGINS
+
