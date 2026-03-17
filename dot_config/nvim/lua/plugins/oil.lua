ddeb29449d48a07c8e4ac0ac051651e13ba6cef1
monapo @ ubuntu <mopapo@the.quietest.linux>
2026-03-17 09:58:22 +0900

    init: oil.lua

---
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,10 @@
+return {
+    "stevearc/oil.nvim",
+    -- dependencies に mini.icons を書かない
+    opts = {
+        -- "icon" を除外
+        -- `help oil-columns`
+        columns = { "mtime" },
+    },
+}
+
