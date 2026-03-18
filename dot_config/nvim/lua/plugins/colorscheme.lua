4d7a9b963ebac6c6dcde261362a377858966bce7
monapo @ ubuntu <mopapo@the.quietest.linux>
2026-03-18 13:52:20 +0900

    init: colorscheme.lua

---
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,38 @@
+-- 使いたい color は `lazy = ture` にする。
+-- config 内で colorscheme を適用しない
+-- お試しは `lazy = false` (ただし :colorscheme から補完は出来ない)
+
+return {
+    {
+        "folke/tokyonight.nvim",
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+
+        opts = {
+            styles = {
+                keywords = { italic = false },
+                comments = { italic = false },
+            },
+        },
+
+        config = function()
+            -- load the colorscheme here
+            vim.cmd('colorscheme tokyonight')
+            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        end,
+
+    },
+
+    {
+        "Kopihue/one-dark-pro-max",
+        lazy = true,
+        -- event = "VeryLazy",
+        name = "one-dark-pro-max",
+        opts = {
+            transparency = false,
+            bold = true,
+            italic = false,
+        },
+    },
+}
+
