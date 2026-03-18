894b2a243136ca98072b292b27ee275239b95cf1
monapo @ ubuntu <mopapo@the.quietest.linux>
2026-03-18 13:42:27 +0900

    fix: it's move

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,31 +1,49 @@
--- req: git, cc
+-- req: git, clang, build-essential
 
 return {
     'nvim-treesitter/nvim-treesitter',
     dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
-    build = ":TSUpdate",
+    lazy = false,
+    build = ':TSUpdate',
 
     config = function()
-        local configs = require("nvim-treesitter.config")
-        configs.setup({
-            highlight = { enable = true, },
-            indent    = { enable = true, },
-            auto_install = true,
+        local ts = require("nvim-treesitter")
+        local parsers = {
+            "rust",
+            "python",
+            "lua",
+            "bash",
+            "zsh",
+            "markdown",
+            "markdown_inline",
+            "json",
+            "toml",
+            "c",
+            "cpp",
+            "query",
+            "vim",
+            "vimdoc",
+            "tmux",
+        }
 
-            ensure_installed = {
-                "lua",
-                "c",
-                "cpp",
-                "python",
-                "query",
-                "vim",
-                "markdown",
-                "markdown_inline",
-                "vimdoc",
-                "bash",
-                "tmux",
-                "rust",
-            },
+        for _, parser in ipairs(parsers) do
+            ts.install(parser)
+        end
+
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = parsers,
+            callback = function()
+                vim.treesitter.start()
+
+                vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+                vim.wo[0][0].foldmethod = 'expr'
+                vim.opt.foldenable = true
+                vim.opt.foldlevel = 99
+
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
         })
     end
 }
+
+
