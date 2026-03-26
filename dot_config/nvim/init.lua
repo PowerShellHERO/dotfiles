641d95f924d5b768b5adcec1efa3950ae8ac0624
monapo @ ubuntu <mopapo@the.quietest.linux>
2026-03-26 15:00:21 +0900

    m: add Set Search Pattern, complate suggest(cmd)

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -9,6 +9,7 @@
     -- d, new dir
     -- %, new file
 
+
 function pp(v) print(vim.inspect(v)) end -- pretty print table
 
 require('config.options')
@@ -17,6 +18,7 @@ require('config.lazy')
 
 require('user.ime2')
 require('user.file_navigation').setup()
+
 
 -- YANK {{{
 local binpath = '~/bin/'
@@ -38,6 +40,7 @@ vim.keymap.set({ 'n', 'x' }, 'x', '"_x', { desc = 'Delete using blackhole regist
 
 -- }}}
 
+
 -- abbreviations {{{
 
 vim.cmd([[
@@ -51,10 +54,38 @@ cabbr ya %y
 -- Untill Lazy Install
 -- vim.cmd.colorscheme("unokai")
 
+
 -- JAPANESE WRITING (vip gq)
 vim.opt.textwidth  = 76
 vim.opt.formatexpr = "autofmt#japanese#formatexpr()"
 -- vim.opt.wrap = true
 
 vim.keymap.set('n', '<leader>i', 'gg=G<C-o>', { desc = 'Re-Indent buffer' })
+
+vim.opt.wildmode = "noselect"
+vim.api.nvim_create_autocmd("CmdlineChanged", {
+  pattern = ":",
+  callback = function ()
+    vim.fn.wildtrigger()
+  end
+})
+
+-- Set Search Pattern
+vim.keymap.set('n', '<Leader>l', function()
+    local ft = vim.bo.filetype
+    local pattern = ""
+
+    if ft == "lua" then
+        -- Lua: require('...') の中身を選択
+        pattern = [[\v^require\('\zs.*\ze'\)]]
+    else
+        -- markdownlink URL
+        pattern = [[\v\]\(\zs.*\ze\)]]
+    end
+
+    -- 検索レジスタ（/）にパターンをセット
+    vim.fn.setreg("/", pattern)
+
+    print("Set Search pattern for" .. ft)
+end, { desc = "Set search pattern based on filetype" })
 
